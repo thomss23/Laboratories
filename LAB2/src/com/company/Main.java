9206a6d0b03c48c705ff6d78d21bea4b51e6582b
thomss23 <71756478+thomss23@users.noreply.github.com>
2020-12-08 11:01:46 +0200

    Update Main.java

--- LAB2/src/com/company/Main.java
+++ LAB2/src/com/company/Main.java
@@ -4,6 +4,10 @@ package com.company;
 
 public class Main {
     public static void main (String[] args) {
+        Book b1 = new Book("Morometii", "Marin Preda", 1955);
+        System.out.println(b1);
 
+        Book b2 = new Book("Levantul", "Mircea Cărtărescu", 1990);
+        System.out.println(b2);
     }
 }
